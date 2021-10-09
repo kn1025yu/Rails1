dc1000fdd38bbcd97dc8ede76e71789b5d031bbd
kn1025yu <91664882+kn1025yu@users.noreply.github.com>
2021-10-09 16:08:12 +0000

    [finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ca6e6385358854b9cf2f2b4fb77117c01e6dac6ab97fb3ef8c48ada5a35510cb3196910a81cb7f11b537b5240dfe9d54bee4e5947e7bb5343bb8b89389eaf3b2'
